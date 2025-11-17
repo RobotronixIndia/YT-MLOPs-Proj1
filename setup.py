203bfc51ff827a111c4fa6f3129444d90d1efbef
PreetiSohni <preeti.sohni.robotronix@gmail.com>
2025-11-17 12:47:22 +0530

    Project structre ready, myenv ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Preeti Sohni",
+    author_email="preeti.sohni.robotronix@gmail.com",
+    packages=find_packages()
+)
